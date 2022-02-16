d360dd2f2b7399df15168959fccf6a2a9f89b1fb
serine222 <ssryn77@gmail.com>
2022-02-16 20:01:48 +0100

    created the main project structure

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+
+
+$('.cycle-slideshow').cycle({
+    speed: "1000",
+    manualSpeed: "2000",
+    
+});
+
+
+
+$(document).ready(function(){  
+    $(".card").click(function(){  
+        alert("نعتذر لعدم وجود معلومات متاحة");  
+    });  
+});  
